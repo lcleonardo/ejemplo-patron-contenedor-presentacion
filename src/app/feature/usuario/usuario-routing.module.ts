82ceab5f84bf498ad943f323500f78d716891167
leonardo.cruz <leonardo.cruz@ceiba.com.co>
2021-09-27 10:45:03 -0500

    Ya funciona el modulo de usuarios

--- src/app/feature/usuario/usuario-routing.module.ts
+++ src/app/feature/usuario/usuario-routing.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+import { UsuarioComponent } from './components/usuario/usuario.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: UsuarioComponent,
+  },
+]
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class UsuarioRoutingModule { }
+export class UsuarioRoutingModule {}
--- src/app/feature/usuario/usuario.module.ts
+++ src/app/feature/usuario/usuario.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common'
 import { UsuarioRoutingModule } from './usuario-routing.module'
 import { ListarUsuarioComponent } from './components/listar-usuario/listar-usuario.component'
 import { UsuarioComponent } from './components/usuario/usuario.component'
-import { FormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms'
 import { GuardarComponent } from './components/guardar/guardar.component'
+import { UsuarioService } from './shared/usuario.service'
 
 @NgModule({
   declarations: [UsuarioComponent, ListarUsuarioComponent, GuardarComponent],
   imports: [FormsModule, CommonModule, UsuarioRoutingModule],
+  providers: [UsuarioService],
 })
 export class UsuarioModule {}
